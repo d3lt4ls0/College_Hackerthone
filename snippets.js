4668487e72d548a6b1cb865eb0068f02d2fce3fd
LALIT SINGH <93564368+d3lt4ls0@users.noreply.github.com>
2026-02-28 11:30:23 +0530

    Add files via upload

---
+++ snippets.js
@@ -0,0 +1,22 @@
+const snippets = [
+
+`for i in range(5):
+    print(i)`,
+
+`function hello(){
+    console.log("Hello World");
+}`,
+
+`int main(){
+    return 0;
+}`,
+
+`while(true){
+    break;
+}`,
+
+`if(x > 10){
+    return x;
+}`
+
+];
